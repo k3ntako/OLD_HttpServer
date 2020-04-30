c0f7db638903c6f7ba65d6da203a817aeedfb45e
Kentaro Kaneki <kentarokaneki@gmail.com>
2020-04-30 11:18:34 -0400

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+  @Test
+  public void something(){
+    assertEquals(true, true);
+  }
+
+}
